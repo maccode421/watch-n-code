f96abc9dc0b9a1d7a8fc239f2fc6af12cd2f23bf
Macc <maccavalli@gmail.com>
2017-11-04 14:23:38 +0000

    first commit: add new file

---
+++ version1-arrays.js
@@ -0,0 +1,14 @@
+item 1, item 2, item 3
+
+'item 1', 'item 2', 'item 3'
+
+['item 1', 'item 2', 'item 3']
+
+var todos = ['item 1', 'item 2', 'item 3']
+console.log(todos)
+console.log('My todos: ', todos)
+
+todos.push('item 4')
+
+
+
